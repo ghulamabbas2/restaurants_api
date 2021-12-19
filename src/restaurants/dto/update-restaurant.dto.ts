6dc7209ad015528352113ea2b42f9a741f2aea13
Ghulam Abbas <ghulamabbas258@gmail.com>
2021-12-20 02:23:28 +0500

    Added validation

--- src/restaurants/dto/update-restaurant.dto.ts
+++ src/restaurants/dto/update-restaurant.dto.ts
@@ -1,10 +1,34 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 import { Category } from '../schemas/restaurant.schema';
 
 export class UpdateRestaurantDto {
+  @IsString()
+  @IsOptional()
   readonly name: string;
+
+  @IsString()
+  @IsOptional()
   readonly description: string;
+
+  @IsEmail({}, { message: 'Please enter correct email address' })
+  @IsOptional()
   readonly email: string;
+
+  @IsPhoneNumber('US')
+  @IsOptional()
   readonly phoneNo: number;
+
+  @IsString()
+  @IsOptional()
   readonly address: string;
+
+  @IsEnum(Category, { message: 'Please enter correct category' })
+  @IsOptional()
   readonly category: Category;
 }
